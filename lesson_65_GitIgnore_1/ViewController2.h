b38b8a8911cd9fc6743230816695b61c9140e23e
Vladimir <>
2017-11-08 20:30:18 +0200

    add VC2

---
+++ lesson_65_GitIgnore_1/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  lesson_65_GitIgnore_1
+//
+//  Created by Vladimir on 08.11.17.
+//  Copyright © 2017 iOS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController2 : ViewController
+
+@end
