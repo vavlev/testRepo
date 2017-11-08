3a32dcf7cfc16e0775ae4df00c4e087099882e8b
Vladimir <Vladimir@MacBook-Pro-Vladimir.local>
2017-11-08 20:32:49 +0200

    add VC3

---
+++ lesson_65_GitIgnore_1/ViewController3.h
@@ -0,0 +1,13 @@
+//
+//  ViewController3.h
+//  lesson_65_GitIgnore_1
+//
+//  Created by Vladimir on 08.11.17.
+//  Copyright © 2017 iOS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController3 : ViewController
+
+@end
